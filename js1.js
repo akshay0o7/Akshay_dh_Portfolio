800a69b69a334a88cc4246e1d0df4ba1724a5581
akshay0o7 <akshaydh8133@gmail.com>
2023-11-03 01:34:51 +0530

    made few changes to scroll of work-more and added google sheet

--- js1.js
+++ js1.js
@@ -1,23 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const seeMoreBtn = document.getElementById("see-more-btn");
   const seeLessBtn = document.getElementById("see-less-btn");
   const workMoreSection = document.getElementById("work-more");
+  let clickCount = 0;
 
-  seeMoreBtn.addEventListener("click", function() {
+  seeMoreBtn.addEventListener("click", function () {
+    clickCount++;
     workMoreSection.classList.add("visible"); // Add the 'visible' class
     seeMoreBtn.style.display = "none";
     seeLessBtn.style.display = "block";
 
-    // Scroll to the "See Less" button
-    seeLessBtn.scrollIntoView({ behavior: "smooth" });
+        // Scroll to the corresponding work item based on clickCount
+    const workItemToScroll = document.getElementById(`work-item-${clickCount}`);
+    if (workItemToScroll) {
+      workItemToScroll.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+    }
   });
 
-  seeLessBtn.addEventListener("click", function() {
+
+  seeLessBtn.addEventListener("click", function () {
     workMoreSection.classList.remove("visible"); // Remove the 'visible' class
     seeMoreBtn.style.display = "block";
     seeLessBtn.style.display = "none";
 
     // Scroll to the "Work" section
     document.getElementById("work").scrollIntoView({ behavior: "smooth" });
+
+    clickCount = 0
   });
 });
