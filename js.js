9cb085ea0b7abfbddef1b779391f25edf94c7ce0
akshay0o7 <139908036+akshay0o7@users.noreply.github.com>
2024-02-19 12:31:25 +0530

    home page display change

--- js.js
+++ js.js
@@ -2,39 +2,38 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-    console.log(tabname)
+  console.log(tabname);
   for (tablink of tablinks) {
-      tablink.classList.remove("active-link");
-      console.log("tablink ----> ", tablink)
+    tablink.classList.remove("active-link");
+    console.log("tablink ----> ", tablink);
   }
   for (tabcontent of tabcontents) {
-      tabcontent.classList.remove("active-tab");
-      console.log("tabcontent ----> ", tabcontent)
+    tabcontent.classList.remove("active-tab");
+    console.log("tabcontent ----> ", tabcontent);
   }
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
 
-var sidemeu =document.getElementById("sidemenu");
-    function openmenu(){
-        sidemeu.style.right = "0";
-    }
-    function closemenu(){
-        sidemeu.style.right = "-200px";
-    }
+var sidemeu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemeu.style.right = "0";
+}
+function closemenu() {
+  sidemeu.style.right = "-200px";
+}
 
-    const roleText = document.getElementById("roleText");
+const roleText = document.getElementById("roleText");
+const spans = roleText.getElementsByTagName("span");
+let currentRoleIndex = 0;
 
-    function toggleRoles() {
-      const spans = roleText.getElementsByTagName("span");
-      for (let i = 0; i < spans.length; i++) {
-        if (spans[i].style.display === "none") {
-          spans[i].style.display = "inline";
-        } else {
-          spans[i].style.display = "none";
-        }
-      }
-    }
-    
-    setInterval(toggleRoles, 3000); // Alternate between roles every 3 seconds
-    
+function toggleRoles() {
+  spans[currentRoleIndex].style.display = "none";
+  currentRoleIndex = (currentRoleIndex + 1) % spans.length;
+  spans[currentRoleIndex].style.display = "inline";
+}
+
+// Start with the first role displayed
+spans[currentRoleIndex].style.display = "inline";
+
+setInterval(toggleRoles, 2500); // Alternate between roles every 2.5 seconds
